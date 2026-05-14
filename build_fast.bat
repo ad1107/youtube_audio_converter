53d7adfc530c8ffdf9e90770ea7332ece9efa04a
ad1107 <nguyenanhducxmen@gmail.com>
2026-05-14 22:43:33 +0700

    v1.0.1 - better pyinstaller support

--- build_fast.bat
+++ build_fast.bat
@@ -3,17 +3,7 @@ echo Installing PyInstaller...
 pip install pyinstaller
 
 echo Building the application with PyInstaller...
-pyinstaller ^
-  --noconfirm ^
-  --onefile ^
-  --windowed ^
-  --hidden-import=yt_dlp ^
-  --name "AudioBookConverter" ^
-  --exclude-module=pandas ^
-  --exclude-module=numpy ^
-  --exclude-module=scipy ^
-  --exclude-module=matplotlib ^
-  main.py
+pyinstaller --noconfirm AudioBookConverter.spec
 
 echo Build complete! Your AudioBookConverter.exe will be inside the "dist" folder.
 pause
