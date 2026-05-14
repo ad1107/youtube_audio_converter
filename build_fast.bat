51d0d795a60154ee3644c54349d0ff9dc1ed54e1
ad1107 <nguyenanhducxmen@gmail.com>
2026-05-14 22:45:34 +0700

    new name

--- build_fast.bat
+++ build_fast.bat
@@ -1,9 +1,5 @@
-@echo off
-echo Installing PyInstaller...
-pip install pyinstaller
-
 echo Building the application with PyInstaller...
-pyinstaller --noconfirm AudioBookConverter.spec
+pyinstaller --noconfirm YoutubeAudioConverter.spec
 
-echo Build complete! Your AudioBookConverter.exe will be inside the "dist" folder.
+echo Build complete! Your YoutubeAudioConverter.exe will be inside the "dist" folder.
 pause
